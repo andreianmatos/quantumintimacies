f3035e8e6bcfaeabef59e061e5f72ac58043b33d
Andreia Matos <58214313+andreianmatos@users.noreply.github.com>
2025-04-15 14:29:42 +0100

    start

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function preload() {
+  // Load the red envelope images
+  image1 = loadImage('data/imgs/image1.png');
+  image2 = loadImage('data/imgs/image2.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noFill();
+
+}
+
+function draw() {
+ 
+}
